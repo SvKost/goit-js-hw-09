e09bc87d03833c7b0865ce3cfd72b4994346fb5d
SvKost <skost0634610557@gmail.com>
2024-01-05 18:00:08 +0200

    Changed code logic

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,57 +1,40 @@
 const STORAGE_KEY = 'feedback-form-state';
-const STORAGE_KEY_EMAIL = 'feedback-email-state';
-const STORAGE_KEY_MESSAGE = 'feedback-message-state';
-
 const form = document.querySelector('.feedback-form');
-const email = document.querySelector('input');
-const textarea = document.querySelector('textarea');
 
-form.addEventListener('submit', onFormSubmit);
-textarea.addEventListener('input', onTextareaInput);
-email.addEventListener('input', onEmailInput);
+form.addEventListener('input', saveFormDataToStorage);
 
-fillInTextarea();
-fillInEmail();
+function saveFormDataToStorage() {
+  const formData = {
+    email: form.querySelector('input[name="email"]').value.trim(),
+    textarea: form.querySelector('textarea[name="message"]').value.trim(),
+  };
 
-function onFormSubmit(event) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function loadFormDataFromStorage() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+  fillInForm('email', savedData.email);
+  fillInForm('message', savedData.textarea);
+}
+
+function fillInForm(fieldName, savedValue) {
+  const formField = form.querySelector(`[name="${fieldName}"]`);
+
+  if (savedValue) {
+    formField.value = savedValue;
+  }
+}
+
+loadFormDataFromStorage();
+
+form.addEventListener('submit', event => {
   event.preventDefault();
 
-  const inputEmail = localStorage.getItem(STORAGE_KEY_EMAIL);
-  const inputMsg = localStorage.getItem(STORAGE_KEY_MESSAGE);
+  const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+  console.log(formData.email);
+  console.log(formData.textarea);
 
-  if (inputEmail && inputMsg) {
-    localStorage.removeItem(STORAGE_KEY_EMAIL);
-    localStorage.removeItem(STORAGE_KEY_MESSAGE);
-    event.currentTarget.reset();
-  } else {
-    alert('Please fill in all fields!');
-  }
-}
-
-function onTextareaInput(event) {
-  const message = event.currentTarget.value.trim();
-
-  localStorage.setItem(STORAGE_KEY_MESSAGE, message);
-}
-
-function onEmailInput(event) {
-  const userEmail = event.currentTarget.value.trim();
-
-  localStorage.setItem(STORAGE_KEY_EMAIL, userEmail);
-}
-
-function fillInTextarea() {
-  const savedMsg = localStorage.getItem(STORAGE_KEY_MESSAGE);
-
-  if (savedMsg) {
-    textarea.value = savedMsg;
-  }
-}
-
-function fillInEmail() {
-  const savedEmail = localStorage.getItem(STORAGE_KEY_EMAIL);
-
-  if (savedEmail) {
-    email.value = savedEmail;
-  }
-}
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+});
